0cdb5041d4d634502c1a44b46ba5e4dc78588923
Lanyta Setyani Gunawan <94893138+MeiLing19@users.noreply.github.com>
2022-02-15 03:34:35 +0700

    Create latihan2.c

---
+++ SEARCHING/SEQUENTIAL SEARCHING/latihan2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int banyakData, i, cari;
+    int ketemu = 0;
+
+    printf("Pencarian Data dengan Algoritma Sequential Search\n\n");
+    printf("Input Banyak Data\t: ");
+    scanf("%d", &banyakData);
+    int data[banyakData];
+
+    printf("Input Kumpulan Data\t: ");
+    for(i = 0; i < banyakData; i++)
+    {
+        scanf("%d", &data[i]);
+    }
+
+    printf("Input Data yang Dicari  : ");
+    scanf("%d", &cari);
+
+    for(i = 0; i < banyakData; i++)
+    {
+        if(cari == data[i])
+        {
+            ketemu = 1;
+            printf("Hasil Pencarian \t: Data Ditemukan pada Indeks ke - %d\n", i);
+            break;
+        }
+    }
+
+    if(ketemu == 0)
+        printf("Hasil Pencarian \t  : Data Tidak Ditemukan\n");
+
+    return 0;
+}
