faf8220e225dace8a20aa4f5802cfe5713b69920
Lanyta Setyani Gunawan <94893138+MeiLing19@users.noreply.github.com>
2022-02-18 10:37:52 +0700

    Create latihan1.c

---
+++ STRUCT/latihan1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+typedef struct Mahasiswa {char NIM[9]; char nama[30]; float ipk;};
+
+void main()
+{
+    struct Mahasiswa mhs;
+    system("cls");
+    printf("NIM = ");
+    scanf("%s", &mhs.NIM);
+    printf("Nama = ");
+    scanf("%s", &mhs.nama);
+    printf("IPK = ");
+    scanf("%f", &mhs.ipk);
+    printf("Data Anda :\n");
+    printf("NIM : %s\n", mhs.NIM);
+    printf("Nama : %s\n", mhs.nama);
+    printf("IPK : %f\n", mhs.ipk);
+
+    getch();
+}
