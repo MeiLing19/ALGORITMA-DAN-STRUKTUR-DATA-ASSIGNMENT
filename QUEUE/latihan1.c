4f8d561699782fd8f791de30a7853c172cafecd4
Lanyta Setyani Gunawan <94893138+MeiLing19@users.noreply.github.com>
2022-04-18 22:57:57 +0700

    Update latihan1.c

--- QUEUE/latihan1.c
+++ QUEUE/latihan1.c
@@ -6,61 +6,71 @@ typedef struct
     int data[MAX];
     int head;
     int tail;
-}Queue;
+} Queue;
 
 Queue antrian;
 
-void Awal(){
-    antrian.head=antrian.tail=-1;
+void Awal()
+{
+    antrian.head = antrian.tail = -1;
 }
 
-int Kosong(){
-    if(antrian.tail == -1){
+int Kosong()
+{
+    if (antrian.tail == -1)
+    {
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
 
-int Penuh(){
-    if (antrian.tail == MAX-1)
+int Penuh()
+{
+    if (antrian.tail == MAX - 1)
     {
         return 1;
-    }else{
+    }
+    else
+    {
         return 0;
     }
-    
 }
 
-int Enqueue(int data){
-    if (Kosong()==1)
+int Enqueue(int data)
+{
+    if (Kosong() == 1)
     {
-        antrian.head = antrian.tail=0;
-        antrian.data[antrian.tail]=data;
+        antrian.head = antrian.tail = 0;
+        antrian.data[antrian.tail] = data;
         printf("%d masuk!\n", antrian.data[antrian.tail]);
         void Tampil();
         {
-            if (Kosong()==0)
+            if (Kosong() == 0)
             {
                 for (int i = antrian.head; i < antrian.tail; i++)
                 {
                     printf("%d ", antrian.data[i]);
                 }
-            } else{
+            }
+            else
+            {
                 printf("Isi antrian kosong!");
             }
-            
-        }           
-    } else if (Penuh()==0)
+        }
+    }
+    else if (Penuh() == 0)
     {
         antrian.tail++;
-        antrian.data[antrian.tail]=data;
+        antrian.data[antrian.tail] = data;
         printf("%d masuk!\n", antrian.data[antrian.tail]);
-    }  
+    }
 }
 
-int Dequeue(){
+int Dequeue()
+{
     int antri = antrian.data[antrian.head];
     for (int i = antrian.head; i < antrian.tail; i++)
     {
@@ -70,32 +80,35 @@ int Dequeue(){
     return antri;
 }
 
-void Tampil(){
-    if (Kosong()==0)
+void Tampil()
+{
+    if (Kosong() == 0)
     {
         for (int i = antrian.head; i <= antrian.tail; i++)
         {
             printf("%d ", antrian.data[i]);
         }
     }
-    else {
+    else
+    {
         printf("Tidak ada data dalam antrian!");
     }
-    
 }
 
-void Clear(){
-    antrian.head=antrian.tail=-1;
+void Clear()
+{
+    antrian.head = antrian.tail = -1;
     printf("Data sudah dibersihkan!");
 }
 
-void main(){
+void main()
+{
     int choice, data;
     Awal();
     do
     {
         printf("\n");
-        printf("1. Enqueue\n"); //masuk satu data
+        printf("1. Enqueue\n"); // masuk satu data
         printf("2. Dequeue\n"); // keluar satu data
         printf("3. Lihat isi antrian\n");
         printf("4. Hapus isi antrian\n");
@@ -121,6 +134,6 @@ void main(){
             break;
         default:
             break;
-        }    
+        }
     } while (choice != 5);
 }
